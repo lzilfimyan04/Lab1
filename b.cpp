05ba8748c44db8551d2aa5fd6fa2874c34418e15
lzilfimyan04 <lzilfimyan@gmail.com>
2026-03-03 11:11:37 +0400

    Create b.cpp

---
+++ b.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    cout << "Running file B" << endl;
+
+    if (argc > 1) {
+        cout << "User output: " << argv[1] << endl;
+    }
+
+    return 0;
+}
